8a411cf9cb754c3175f3ad975704d3125fb3874c
Lukas <Lukas.Siegmueller@yahoo.com>
2021-09-17 20:00:46 +0200

    Including function and export .csv for plotting

--- annuitycalc.py
+++ annuitycalc.py
@@ -1,6 +1,6 @@
 import csv
 
-def calc_annuity_monthly(darlehenssumme, zinssatz, tilgungssatz, monate):
+def calc_annuity_monthly_tableau(darlehenssumme, zinssatz, tilgungssatz, monate):
 
     # Listen für return
     fields = ["monat", "zinsen", "tilgung"]
@@ -34,19 +34,56 @@ def calc_annuity_monthly(darlehenssumme, zinssatz, tilgungssatz, monate):
         i += 1
     
     # Export .csv
-    with open('data.csv', 'w') as f:
+    with open('tableau.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
         writer.writerows(rows)
 
+def calc_annuity_monthly_graph(darlehenssumme, zinssatz, tilgungssatz, monate):
 
+    # Listen für return
+    fields = ["zinsen", "tilgung"]
+    rows = []
+
+    # Ermittlung Restschuld  >> Zinsen auf Restschuld >> neue Tilgung >> ..
+    i = 1
+    while i < int(monate) + 1:
+        if i == 1:
+            rate_tilgung_mtl = round((darlehenssumme * tilgungssatz)/12, 2)
+            rate_zinsen_mtl = round((darlehenssumme * zinssatz)/12, 2)
+            rate_gesamt_mtl =  rate_tilgung_mtl + rate_zinsen_mtl
+            rows_1 = []
+            rows_1.append(rate_zinsen_mtl)
+            rows_1.append(rate_tilgung_mtl)
+            rows.append(rows_1)
+            i += 1
+            continue
+
+        darlehenssumme = darlehenssumme - rate_tilgung_mtl
+        rate_zinsen_mtl = round((darlehenssumme * zinssatz)/12, 2)
+        rate_tilgung_mtl = round(rate_gesamt_mtl - rate_zinsen_mtl, 2)
+        if rate_gesamt_mtl != rate_zinsen_mtl + rate_tilgung_mtl:
+            break
+        rows_1 = []
+        rows_1.append(rate_zinsen_mtl)
+        rows_1.append(rate_tilgung_mtl)
+        rows.append(rows_1)
+        i += 1
+    
+    # Export .csv
+    with open('graph.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+        writer.writerows(rows)
 
 # Ausführung
 darlehen_sum = 311000
 zins_percent = 0.0183
 tilgung_percent = 0.03
 
-calc_annuity_monthly(darlehen_sum, zins_percent, tilgung_percent, monate=12)
+calc_annuity_monthly_tableau(darlehen_sum, zins_percent, tilgung_percent, monate=360)
+calc_annuity_monthly_graph(darlehen_sum, zins_percent, tilgung_percent, monate=360)
+
 
 
 
